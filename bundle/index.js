242d25ef8bd49bd1827809e6b354257987d6c9d7
Fixron Maulana <fikronmaulana27@gmail.com>
2025-02-03 05:31:29 +0700

    Learn variable (basic + array biasa & array tupple )

---
+++ bundle/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let namaSaya = "Fikron Maulana";
+let username = 123;
+let isLife = true;
+//array biasa
+let pacarSaya;
+pacarSaya = ["ukthi", "yalil-yalil"];
+//tuple array
+let selingkuhanSaya;
+selingkuhanSaya = ["agnes", 1101, true];
+username = 1922;
+isLife = false;
+console.log({ namaSaya });
+console.log({ username });
+console.log({ isLife });
+console.log({ pacarSaya });
+console.log({ selingkuhanSaya });
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+let namaSaya: string = "Fikron Maulana";
+
+let username: number = 123;
+
+let isLife: boolean = true;
+
+//array biasa
+let pacarSaya: string[];
+pacarSaya = ["ukthi", "yalil-yalil"];
+
+//tuple array
+let selingkuhanSaya: [string, number, boolean];
+selingkuhanSaya = ["agnes", 1101, true];
+
+username = 1922;
+isLife = false;
+
+console.log({ namaSaya });
+console.log({ username });
+console.log({ isLife });
+console.log({ pacarSaya });
+console.log({ selingkuhanSaya });
